ea7787d83b8dbc2db5345847c324c88a169b3c14
Giruba Beulah SE <gibeulah@microsoft.com>
2019-03-05 08:02:08 +0530

    Add project files.

---
+++ ActivitySelectionProblem/Activity.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ActivitySelectionProblem
+{
+    class Activity
+    {
+        int StartTime;
+        int EndTime;
+
+        public Activity() { }
+
+        public Activity(int startTime, int endTime, int profit) {
+            StartTime = startTime;
+            EndTime = endTime;
+        }
+
+        public int GetActivityStartTime() {
+            return StartTime;
+        }
+
+        public int GetActivityEndTime() {
+            return EndTime;
+        }
+
+
+        public void SetActivityStartTime(int startTime) {
+            StartTime = startTime;
+        }
+
+
+        public void SetActivityEndTime(int endTime)
+        {
+            EndTime = endTime;
+        }
+        
+    }
+}
---
+++ ActivitySelectionProblem/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace ActivitySelectionProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Activity Selection Problem!");
+            Console.WriteLine("---------------------------");
+
+            Activity[] activities = GetActivities();
+            PrintMaxActivitiesByAPerson(activities);
+
+            Console.ReadLine();
+        }
+
+        private static Activity[] GetActivities()
+        {
+            Activity[] activities = null;
+
+            Console.WriteLine("Enter the number of activities");
+            try
+            {
+                int noActivities = int.Parse(Console.ReadLine());
+                activities = new Activity[noActivities];
+                for (int index = 0; index < noActivities; index++) {
+                    Console.WriteLine("Enter the start-time, end-time " +
+                        "separated by space");
+                    String[] allElements = Console.ReadLine().Split(' ');
+                    activities[index] = new Activity();
+                    activities[index].SetActivityStartTime(int.Parse(allElements[0]));
+                    activities[index].SetActivityEndTime(int.Parse(allElements[1]));
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine("Thrown exception is " + exception.Message);
+            }
+
+            return activities;
+        }
+
+        private static void PrintMaxActivitiesByAPerson(Activity[] activities) {
+            Console.WriteLine("Chosen Activity 0:"+activities[0].GetActivityStartTime()
+                +"->"+activities[0].GetActivityEndTime());
+
+            int formerActivity = 0;
+
+            for (int index = 1; index < activities.Length; index++) {
+                if (activities[index].GetActivityStartTime() >=
+                        activities[formerActivity].GetActivityEndTime()) {
+                    formerActivity = index;
+                    Console.WriteLine("Chosen Activity " + index +
+                        ":" + activities[index].GetActivityStartTime() + "->" +
+                        activities[index].GetActivityEndTime());
+                }
+            }
+        }
+    }
+}
